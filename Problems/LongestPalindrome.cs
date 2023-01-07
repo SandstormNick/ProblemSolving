ad2f66324da6be6bf5855cf6e87e3ee2f65ed392
SandstormNick <gibbensnick@gmail.com>
2023-01-07 11:25:02 +0200

    Working on the longestpalin method

--- Problems/LongestPalindrome.cs
+++ Problems/LongestPalindrome.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace Problems
 {
-    internal class LongestPalindrome
+    public  class LongestPalindrome
     {
+        public string longestPalin(string inputString)
+        {
+            //aaaabbaa
+            string longestPalindrome = inputString.Substring(0, 1);
+            char[] chars = inputString.ToCharArray();
+
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i] == chars[i + 1] && chars[i] != chars.Length - 1)
+                {
+                    bool stillPalin = true;
+                    //longestPalin = inputString.Substring() --will need an if check to see if its necessary to assign to longestPalin
+                    int leftChar = i -1;
+                    int rightChar = i + 2;
+
+                    while (stillPalin && leftChar > -1 && rightChar < chars.Length)
+                    {
+                        
+                        stillPalin = false;
+                    }
+                }
+
+                if (chars[i] == chars[i+ 2] && chars[i] != chars.Length - 2)
+                {
+                    bool stillPalin = true;
+                }
+                //check for grouping of 2
+
+                //check for grouping of 3
+
+                // =if it finds 2/3 then start another loop and look for matching chars on either side
+
+                //the array limits need to be taken into account as well
+            }
+
+            return inputString;
+        }
     }
 }

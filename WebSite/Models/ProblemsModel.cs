15dcef74bb077a6d7202c7cdc39d92f9a40e75e8
SandstormNick <gibbensnick@gmail.com>
2023-01-08 17:42:35 +0200

    Added LongestPalindrome to website

--- WebSite/Models/ProblemsModel.cs
+++ WebSite/Models/ProblemsModel.cs
@@ -5,7 +5,7 @@ namespace WebSite.Models
     public class ProblemsModel
     {
         [DisplayName("Answer:")]
-        public string? Answer { get; set; }
+        public string? StringAnswer { get; set; }
         public string? InputString { get; set; }
         public string? ProblemTitle { get; set; }
     }
